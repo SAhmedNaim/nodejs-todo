27f423529524e920c4092879bcd1cf4d64e0d1ff
unknown <naim.ahmed035@gmail.com>
2021-07-09 17:02:30 +0600

    Add some options to resolve mongo connection

--- app/src/mongo.js
+++ app/src/mongo.js
@@ -4,7 +4,12 @@ const HOST = process.env.MONGODB_HOST || "localhost";
 const DATABASE = process.env.MONGODB_DATABASE || "nodejs-todo";
 
 export const uri = `mongodb://${HOST}:27017/${DATABASE}`;
-const options = { useUnifiedTopology: true };
+const options = { 
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+    useFindAndModify: false,
+    useCreateIndex: true
+};
 
 export const connectWithDb = () => {
     mongoose.connect(uri, options, (err, db) => {
